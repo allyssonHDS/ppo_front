1cd666fcea2f65f517394a210e7c848e615ef3b7
allyssonHDS <allysson191298@gmail.com>
2022-06-21 16:53:43 -0300

    criação de dashboard
    dashboard/barra lateral/topo pagina

--- PPO/src/app/app.module.ts
+++ PPO/src/app/app.module.ts
@@ -8,17 +8,42 @@ import { HeaderComponent } from './componts/header/header.component';
 import { SidebarComponent } from './componts/sidebar/sidebar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+/* Angular materia */
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { NevigationComponent } from './nevigation/nevigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    NevigationComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
+    LayoutModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ PPO/src/app/nevigation/nevigation.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-nevigation',
+  templateUrl: './nevigation.component.html',
+  styleUrls: ['./nevigation.component.css']
+})
+export class NevigationComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
