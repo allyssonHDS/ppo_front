b9922afd892e1e907dd0e9085896097cb4050f6b
allyssonHDS <allysson191298@gmail.com>
2022-06-21 15:19:38 -0300

    add material

--- PPO/src/app/app.module.ts
+++ PPO/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './componts/footer/footer.component';
+import { HeaderComponent } from './componts/header/header.component';
+import { SidebarComponent } from './componts/sidebar/sidebar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
