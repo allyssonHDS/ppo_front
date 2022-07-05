5be342e39024442b107eca6c7f2f86f99b1dade2
allyssonHDS <allysson191298@gmail.com>
2022-07-05 11:49:31 -0300

    Co-authored-by: CharlesVilela <CharlesVilela@users.noreply.github.com> Co-authored-by: Bruno Henrique <HenriqueBSS@users.noreply.github.com>

---
+++ PPO/src/app/shared/service/user.service.ts
@@ -0,0 +1,24 @@
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class UserService {
+
+    apiUrl = 'http://localhost:3100/usuario';
+
+    httpOptions = {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json'
+        })
+    };
+
+    constructor(
+        private httpClient: HttpClient
+    ) { }
+
+    public getUser(): Observable<ResponsePageable> {
+        return this.httpClient.get<ResponsePageable>(this.apiUrl + user)
+    }
+
+}
